5fc0703ec44ac8da038738a7089d8475d9bfdce8
kara <ssctonioloriccardo@gmail.com>
2022-08-21 17:13:32 +0200

    Formatting and setting up to study decorators.

--- ts-generics/src/app.ts
+++ ts-generics/src/app.ts
@@ -14,47 +14,50 @@
 // });
 
 function merge<T extends object, U extends object>(objA: T, objB: U) {
-    return Object.assign(objA,objB);
+    return Object.assign(objA, objB);
 }
 
 // console.log(merge({name: 'Riccardo'},{age: '20'}));
-const mergedObj = merge({name: 'Riccardo'},{age: '20'});
+const mergedObj = merge({ name: 'Riccardo' }, { age: '20' });
 console.log(mergedObj.age);
 
-interface Lengthy{
+interface Lengthy {
     length: number;
 }
 
-function countAndDescribe<T extends Lengthy>(element: T): [T,string ]{
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
     let descriptionText = 'Got no value.';
-    if(element.length > 0){
+    if (element.length > 0) {
         descriptionText = 'Got ' + element.length + ' elements.';
     }
-   return [element, descriptionText];
+    return [element, descriptionText];
 }
 
 console.log(countAndDescribe('Hi there!'));
 
-function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U){
+function extractAndConvert<T extends object, U extends keyof T>(
+    obj: T,
+    key: U
+) {
     return obj[key];
 }
 
-console.log(extractAndConvert({name: 'Riccardo'},'name'));
+console.log(extractAndConvert({ name: 'Riccardo' }, 'name'));
 
-class DataStorage<T extends  string|number|boolean> {
+class DataStorage<T extends string | number | boolean> {
     private data: T[] = [];
 
-    addItem(item: T){
+    addItem(item: T) {
         this.data.push(item);
     }
 
-    removeItem(item: T){
-        if(this.data.indexOf(item) === -1){
-           return;
+    removeItem(item: T) {
+        if (this.data.indexOf(item) === -1) {
+            return;
         }
-        this.data.splice(this.data.indexOf(item),1);
+        this.data.splice(this.data.indexOf(item), 1);
     }
-    getItems(){
+    getItems() {
         return [...this.data];
     }
 }
@@ -81,15 +84,19 @@ console.log(numberStorage.getItems());
 interface CourseGoal {
     title: string;
     description: string;
-    completeUntil: Date
+    completeUntil: Date;
 }
 
-function createCourseGoal(title: string, description: string, date: Date): CourseGoal{
-    let courseGoal: Partial <CourseGoal> = {};
+function createCourseGoal(
+    title: string,
+    description: string,
+    date: Date
+): CourseGoal {
+    let courseGoal: Partial<CourseGoal> = {};
     courseGoal.title = title;
     courseGoal.description = description;
     courseGoal.completeUntil = date;
-   return courseGoal as CourseGoal;
+    return courseGoal as CourseGoal;
 }
 
-const names : Readonly<string[]> = ['Max', 'Anna'];
+const names: Readonly<string[]> = ['Max', 'Anna'];
