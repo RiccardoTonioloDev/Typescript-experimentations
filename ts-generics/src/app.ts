52a552dbb4930160a719502d4609eb4fd7bde96d
kara <ssctonioloriccardo@gmail.com>
2022-08-21 17:05:58 +0200

    Finished studying generics.

--- ts-generics/src/app.ts
+++ ts-generics/src/app.ts
@@ -1,14 +1,95 @@
-const names: Array<string> = []; //Esattamente come definire string[]
+// const names: Array<string> = []; //Esattamente come definire string[]
+//
+// names[0].split(' ');
+//
+// const promise = new Promise<string>((resolve, reject)=>{
+//     setTimeout(()=>{
+//         resolve('This is done');
+//     },2000);
+//     reject('Rejection');
+// });
+//
+// promise.then(data=>{
+//     data.split(' ');
+// });
 
-names[0].split(' ');
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA,objB);
+}
 
-const promise = new Promise<string>((resolve, reject)=>{
-    setTimeout(()=>{
-        resolve('This is done');
-    },2000);
-    reject('Rejection');
-});
+// console.log(merge({name: 'Riccardo'},{age: '20'}));
+const mergedObj = merge({name: 'Riccardo'},{age: '20'});
+console.log(mergedObj.age);
 
-promise.then(data=>{
-    data.split(' ');
-});
+interface Lengthy{
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T,string ]{
+    let descriptionText = 'Got no value.';
+    if(element.length > 0){
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+   return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hi there!'));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U){
+    return obj[key];
+}
+
+console.log(extractAndConvert({name: 'Riccardo'},'name'));
+
+class DataStorage<T extends  string|number|boolean> {
+    private data: T[] = [];
+
+    addItem(item: T){
+        this.data.push(item);
+    }
+
+    removeItem(item: T){
+        if(this.data.indexOf(item) === -1){
+           return;
+        }
+        this.data.splice(this.data.indexOf(item),1);
+    }
+    getItems(){
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Ricky');
+textStorage.addItem('Ricky');
+textStorage.addItem('Ricky');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(3);
+numberStorage.addItem(3);
+numberStorage.addItem(3);
+console.log(numberStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({name: 'Ricky'});
+// objStorage.addItem({name: 'Max'});
+// objStorage.removeItem({name: 'Ricky'});
+//
+// console.log(objStorage.getItems());
+
+interface CourseGoal {
+    title: string;
+    description: string;
+    completeUntil: Date
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal{
+    let courseGoal: Partial <CourseGoal> = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+   return courseGoal as CourseGoal;
+}
+
+const names : Readonly<string[]> = ['Max', 'Anna'];
